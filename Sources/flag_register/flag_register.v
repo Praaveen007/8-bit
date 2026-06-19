b034d40eb72ddf1a3703768d683328e3a6226c70
INDRA2006MG <indrapriyadharshanimg.ec24@bitsathy.ac.in>
2026-06-19 07:50:05 +0530

    Update flag_register.v

--- Sources/flag_register/flag_register.v
+++ Sources/flag_register/flag_register.v
@@ -1,1 +1,25 @@
-dcvf
+module flag_register(
+
+    input wire clk,
+    input wire rst,
+
+    input wire FE,
+
+    input wire [1:0] flags_in,
+
+    output reg [1:0] flags_out
+
+);
+
+always @(posedge clk or posedge rst)
+begin
+
+    if(rst)
+        flags_out <= 2'b00;
+
+    else if(FE)
+        flags_out <= flags_in;
+
+end
+
+endmodule
