1a849520ad965ce20641c71ca9a202d2b2186db2
INDRA2006MG <indrapriyadharshanimg.ec24@bitsathy.ac.in>
2026-06-19 09:50:02 +0530

    Update mar.v

--- Sources/mar/mar.v
+++ Sources/mar/mar.v
@@ -1,1 +1,16 @@
+module mar (
+    input  wire        clk,
+    input  wire        rst,
+    input  wire        MI,
+    input  wire [7:0]  bus_in,
+    output reg  [3:0]  addr_out
+);
 
+    always @(posedge clk) begin
+        if (rst)
+            addr_out <= 4'b0000;
+        else if (MI)
+            addr_out <= bus_in[3:0];
+    end
+
+endmodule
