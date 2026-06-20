b9c504a704f50547a1617befd5ce40c1dedecf3f
Rahul-Sivesh <rahulsivesh11@gmail.com>
2026-06-20 14:07:25 +0530

    Update mar.v

--- Sources/mar/mar.v
+++ Sources/mar/mar.v
@@ -1,10 +1,9 @@
 module mar (
     input  wire        clk,
-    input  wire        rst,      // active HIGH asynchronous
+    input  wire        rst,      
     input  wire        MI,
     input  wire [7:0]  bus_in,
-    output reg  [3:0]  addr_out
-);
+    output reg  [3:0]  addr_out);
     always @(posedge clk or posedge rst) begin
         if (rst)       addr_out <= 4'b0000;
         else if (MI)   addr_out <= bus_in[3:0];
