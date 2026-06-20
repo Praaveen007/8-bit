50104ed03fcc371125466bf0963034fa67b614ec
Rahul-Sivesh <rahulsivesh11@gmail.com>
2026-06-20 09:22:12 +0530

    Update reg_a.v

--- Sources/reg_a/reg_a.v
+++ Sources/reg_a/reg_a.v
@@ -1,25 +1,18 @@
-
 module a_register (
     input  wire        clk,
-    input  wire        rst,     
-    input  wire        AI,      
-    input  wire        AO,      
+    input  wire        rst,
+    input  wire        AI,
+    input  wire        AO,
     input  wire [7:0]  bus_in,
-    output wire [7:0]  bus_out,
-    output wire [7:0]  a_val   
+    output wire [7:0]  a_val
 );
-
     reg [7:0] a;
 
-    always @(posedge clk) begin
-        if (rst)      a <= 8'b0000_0000; 
-        else if (AI)  a <= bus_in;     
+    always @(posedge clk or posedge rst) begin
+        if (rst)      a <= 8'h00;
+        else if (AI)  a <= bus_in;
     end
 
-   
-    assign bus_out = AO ? a : 8'bz;
-
-   
     assign a_val = a;
 
 endmodule
