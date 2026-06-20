34de96d62b2b1b804c1dc1f90e28e144a4cbd9d1
Rahul-Sivesh <rahulsivesh11@gmail.com>
2026-06-20 09:27:11 +0530

    Refactor flag_register module for improved readability

--- Sources/flag_register/flag_register.v
+++ Sources/flag_register/flag_register.v
@@ -1,23 +1,12 @@
-module flag_register(
-
-    input  wire       clk,
-    input  wire       rst,
-    input  wire       FE,
-    input  wire [1:0] flags_in,
-
-    output reg [1:0]  flags_out
-
+module flag_register (
+    input  wire        clk,
+    input  wire        rst,
+    input  wire        FE,
+    input  wire [1:0]  flags_in,
+    output reg  [1:0]  flags_out
 );
-
-always @(posedge clk or posedge rst)
-begin
-
-    if(rst)
-        flags_out <= 2'b00;
-
-    else if(FE)
-        flags_out <= flags_in;
-
-end
-
+    always @(posedge clk or posedge rst) begin
+        if (rst)      flags_out <= 2'b00;
+        else if (FE)  flags_out <= flags_in;
+    end
 endmodule
