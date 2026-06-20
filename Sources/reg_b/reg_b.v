ab5448306713edd2cadcba2b3253d664a9070ad1
INDRA2006MG <indrapriyadharshanimg.ec24@bitsathy.ac.in>
2026-06-20 06:55:36 +0530

    Update reg_b.v

--- Sources/reg_b/reg_b.v
+++ Sources/reg_b/reg_b.v
@@ -1,19 +1,19 @@
 module b_register (
     input  wire        clk,
-    input  wire        rst,     // active HIGH reset
-    input  wire        BI,      // B In - load B from bus
+    input  wire        rst,     
+    input  wire        BI,      
     input  wire [7:0]  bus_in,
-    output wire [7:0]  b_val    // direct connection to ALU (always on)
+    output wire [7:0]  b_val    
 );
 
     reg [7:0] b;
 
     always @(posedge clk) begin
-        if (rst)      b <= 8'b0000_0000; // clear B register on reset
-        else if (BI)  b <= bus_in;        // load new value from bus
+        if (rst)      b <= 8'b0000_0000; 
+        else if (BI)  b <= bus_in;        
     end
 
-    // always feed current B value to ALU (no bus drive for B)
+   
     assign b_val = b;
 
 endmodule
