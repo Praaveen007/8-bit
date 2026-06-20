c5e842ffa0e27b1e570b4bab8158a5fe4856541b
INDRA2006MG <indrapriyadharshanimg.ec24@bitsathy.ac.in>
2026-06-20 06:51:52 +0530

    Update program_counter.v

--- Sources/program_counter/program_counter.v
+++ Sources/program_counter/program_counter.v
@@ -10,17 +10,16 @@ module program_counter (
     reg [3:0] pc;
 
     always @(posedge clk) begin
-        if (rst)       pc <= 4'b0000;  // reset PC to zero
-        else if (CL)   pc <= 4'b0000;  // clear PC to zero
-        else if (CE)   pc <= pc + 1'b1; // increment PC
-        // if none of the above, PC holds its value
+        if (rst)       pc <= 4'b0000;  
+        else if (CL)   pc <= 4'b0000; 
+        else if (CE)   pc <= pc + 1'b1; 
+        
     end
 
-    // PC only drives the bus when CO = 1 (tri-state otherwise)
-    // upper nibble is always 0000 since PC is only 4 bits
+    
     assign bus_out = CO ? {4'b0000, pc} : 8'bz;
 
-    // always show PC value on debug output
+   
     assign pc_val = pc;
 
 endmodule
