44f00a174c97b4e22b93c4b6afb451733f4e61eb
INDRA2006MG <indrapriyadharshanimg.ec24@bitsathy.ac.in>
2026-06-20 06:44:10 +0530

    Update clock_module_tb.v

--- Sources/clock_module/clock_module_tb.v
+++ Sources/clock_module/clock_module_tb.v
@@ -10,7 +10,6 @@ module clock_module_tb;
 
     wire clk_out;
 
-    // DUT
     clock_module uut (
         .clk_100mhz(clk_100mhz),
         .rst(rst),
@@ -20,7 +19,7 @@ module clock_module_tb;
         .clk_out(clk_out)
     );
 
-    // 100 MHz clock (10 ns period)
+  
     initial begin
         clk_100mhz = 0;
         forever #5 clk_100mhz = ~clk_100mhz;
@@ -28,31 +27,29 @@ module clock_module_tb;
 
     initial begin
 
-        // Initialize
+       
         rst         = 1'b1;
-        speed_sel   = 2'b11; // 1 kHz mode (fastest)
+        speed_sel   = 2'b11; 
         manual_step = 1'b0;
         hlt         = 1'b0;
 
-        // Hold reset
+   
         #100;
         rst = 1'b0;
 
-        // Run long enough to see clock divider output
-        #1200000;  // 1.2 ms
+       
+        #1200000;  
 
-        // Manual step test
         manual_step = 1'b1;
         #20;
         manual_step = 1'b0;
 
         #100000;
 
-        // Halt test
         hlt = 1'b1;
         #500000;
 
-        // Resume
+    
         hlt = 1'b0;
         #500000;
 
