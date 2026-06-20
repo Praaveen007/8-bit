73295603db64c71269f33ac23ece919f9ad657f2
INDRA2006MG <indrapriyadharshanimg.ec24@bitsathy.ac.in>
2026-06-20 06:52:20 +0530

    Update program_counter_tb.v

--- Sources/program_counter/program_counter_tb.v
+++ Sources/program_counter/program_counter_tb.v
@@ -21,7 +21,7 @@ module program_counter_tb;
         .pc_val(pc_val)
     );
 
-    // Clock generation
+  
     initial begin
         clk = 0;
         forever #5 clk = ~clk;
@@ -34,41 +34,35 @@ module program_counter_tb;
         CE  = 0;
         CL  = 0;
 
-        // Apply reset
         #10;
         rst = 0;
 
-        // Count 5 times
+    
         CE = 1;
         #50;
 
-        // Stop counting
         CE = 0;
         #10;
 
-        // Put PC onto bus
         CO = 1;
         #10;
 
-        // Remove from bus
         CO = 0;
         #10;
 
-        // Clear PC
         CL = 1;
         #10;
         CL = 0;
 
-        // Show cleared PC on bus
         CO = 1;
         #10;
 
-        // Count 3 more times
+
         CO = 0;
         CE = 1;
         #30;
 
-        // Show PC on bus again
+    
         CE = 0;
         CO = 1;
         #10;
